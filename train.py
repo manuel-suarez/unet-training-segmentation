30c2054a6d12707caed1993fddb863305d94808b
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-26 09:10:52 -0600

    Compare different implementations

--- train.py
+++ train.py
@@ -3,10 +3,7 @@
 
 # import the necessary packages
 from pyimagesearch.dataset import SegmentationDataset
-#from pyimagesearch.model_v1 import UNet
-#from pyimagesearch.model_v2 import UNet
-#from pyimagesearch.model_v3 import build_unet
-#from pyimagesearch.model_v4 import UNet
+from pyimagesearch import model_v1, model_v2, model_v3, model_v4
 import segmentation_models_pytorch as smp
 from pyimagesearch import config
 from torch.nn import BCEWithLogitsLoss
@@ -55,15 +52,110 @@ testDS = SegmentationDataset(imagePaths=testImages, maskPaths=testMasks,
 print(f"[INFO] found {len(trainDS)} examples in the training set...")
 print(f"[INFO] found {len(testDS)} examples in the test set...")
 
-# create the training and test data loaders
-trainLoader = DataLoader(trainDS, shuffle=True,
-	batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
-	num_workers=os.cpu_count())
-testLoader = DataLoader(testDS, shuffle=False,
-	batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
-	num_workers=os.cpu_count())
+def train_model(model, version):
+	# create the training and test data loaders
+	trainLoader = DataLoader(trainDS, shuffle=True,
+		batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
+		num_workers=os.cpu_count())
+	testLoader = DataLoader(testDS, shuffle=False,
+		batch_size=config.BATCH_SIZE, pin_memory=config.PIN_MEMORY,
+		num_workers=os.cpu_count())
 
-# initialize our UNet model
+	# initialize loss function and optimizer
+	lossFunc = BCEWithLogitsLoss()
+	opt = Adam(unet.parameters(), lr=config.INIT_LR)
+
+	# calculate steps per epoch for training and test set
+	trainSteps = len(trainDS) // config.BATCH_SIZE
+	testSteps = len(testDS) // config.BATCH_SIZE
+
+	# initialize a dictionary to store training history
+	H = {"train_loss": [], "test_loss": []}
+
+	# loop over epochs
+	print(f"[INFO] training the network {version}...")
+	startTime = time.time()
+	for e in tqdm(range(config.NUM_EPOCHS)):
+		# set the model in training mode
+		unet.train()
+
+		# initialize the total training and validation loss
+		totalTrainLoss = 0
+		totalTestLoss = 0
+
+		# loop over the training set
+		for (i, (x, y)) in enumerate(trainLoader):
+			# send the input to the device
+			(x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
+
+			# perform a forward pass and calculate the training loss
+			pred = unet(x)
+			loss = lossFunc(pred, y)
+
+			# first, zero out any previously accumulated gradients, then
+			# perform backpropagation, and then update model parameters
+			opt.zero_grad()
+			loss.backward()
+			opt.step()
+
+			# add the loss to the total training loss so far
+			totalTrainLoss += loss
+
+		# switch off autograd
+		with torch.no_grad():
+			# set the model in evaluation mode
+			unet.eval()
+
+			# loop over the validation set
+			for (x, y) in testLoader:
+				# send the input to the device
+				(x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
+
+				# make the predictions and calculate the validation loss
+				pred = unet(x)
+				totalTestLoss += lossFunc(pred, y)
+
+		# calculate the average training and validation loss
+		avgTrainLoss = totalTrainLoss / trainSteps
+		avgTestLoss = totalTestLoss / testSteps
+
+		# update our training history
+		H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
+		H["test_loss"].append(avgTestLoss.cpu().detach().numpy())
+
+		# print the model training and validation information
+		print("[INFO] EPOCH: {}/{}".format(e + 1, config.NUM_EPOCHS))
+		print("Model {}, Train loss: {:.6f}, Test loss: {:.4f}".format(
+			version, avgTrainLoss, avgTestLoss))
+
+	# display the total time needed to perform the training
+	endTime = time.time()
+	print("[INFO] total time taken to train the model {}: {:.2f}s".format(
+		version,
+		endTime - startTime))
+
+	MODEL_PATH = os.path.join(config.BASE_OUTPUT, f"unet_{version}_tgs_salt.pth")
+	PLOT_PATH = os.path.sep.join([config.BASE_OUTPUT, f"plot_unet_{version}.png"])
+
+	# plot the training loss
+	plt.style.use("ggplot")
+	plt.figure()
+	plt.plot(H["train_loss"], label="train_loss")
+	plt.plot(H["test_loss"], label="test_loss")
+	plt.title("Training Loss on Dataset")
+	plt.xlabel("Epoch #")
+	plt.ylabel("Loss")
+	plt.legend(loc="lower left")
+	plt.savefig(PLOT_PATH)
+
+	# serialize the model to disk
+	torch.save(unet, MODEL_PATH)
+
+# initialize our UNet model and train
+train_model(model_v1.UNet(), "v1")
+train_model(model_v2.UNet(), "v2")
+train_model(model_v3.build_unet(), "v3")
+train_model(model_v4.UNet(), "v4")
 #unet = UNet().to(config.DEVICE)
 unet = smp.Unet(
 	encoder_name='resnet34',
@@ -71,89 +163,4 @@ unet = smp.Unet(
 	classes=1,
 	activation=None
 ).to(config.DEVICE)
-
-# initialize loss function and optimizer
-lossFunc = BCEWithLogitsLoss()
-opt = Adam(unet.parameters(), lr=config.INIT_LR)
-
-# calculate steps per epoch for training and test set
-trainSteps = len(trainDS) // config.BATCH_SIZE
-testSteps = len(testDS) // config.BATCH_SIZE
-
-# initialize a dictionary to store training history
-H = {"train_loss": [], "test_loss": []}
-
-# loop over epochs
-print("[INFO] training the network...")
-startTime = time.time()
-for e in tqdm(range(config.NUM_EPOCHS)):
-	# set the model in training mode
-	unet.train()
-
-	# initialize the total training and validation loss
-	totalTrainLoss = 0
-	totalTestLoss = 0
-
-	# loop over the training set
-	for (i, (x, y)) in enumerate(trainLoader):
-		# send the input to the device
-		(x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
-
-		# perform a forward pass and calculate the training loss
-		pred = unet(x)
-		loss = lossFunc(pred, y)
-
-		# first, zero out any previously accumulated gradients, then
-		# perform backpropagation, and then update model parameters
-		opt.zero_grad()
-		loss.backward()
-		opt.step()
-
-		# add the loss to the total training loss so far
-		totalTrainLoss += loss
-
-	# switch off autograd
-	with torch.no_grad():
-		# set the model in evaluation mode
-		unet.eval()
-
-		# loop over the validation set
-		for (x, y) in testLoader:
-			# send the input to the device
-			(x, y) = (x.to(config.DEVICE), y.to(config.DEVICE))
-
-			# make the predictions and calculate the validation loss
-			pred = unet(x)
-			totalTestLoss += lossFunc(pred, y)
-
-	# calculate the average training and validation loss
-	avgTrainLoss = totalTrainLoss / trainSteps
-	avgTestLoss = totalTestLoss / testSteps
-
-	# update our training history
-	H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
-	H["test_loss"].append(avgTestLoss.cpu().detach().numpy())
-
-	# print the model training and validation information
-	print("[INFO] EPOCH: {}/{}".format(e + 1, config.NUM_EPOCHS))
-	print("Train loss: {:.6f}, Test loss: {:.4f}".format(
-		avgTrainLoss, avgTestLoss))
-
-# display the total time needed to perform the training
-endTime = time.time()
-print("[INFO] total time taken to train the model: {:.2f}s".format(
-	endTime - startTime))
-
-# plot the training loss
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(H["train_loss"], label="train_loss")
-plt.plot(H["test_loss"], label="test_loss")
-plt.title("Training Loss on Dataset")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss")
-plt.legend(loc="lower left")
-plt.savefig(config.PLOT_PATH)
-
-# serialize the model to disk
-torch.save(unet, config.MODEL_PATH)
+train_model(unet, "smp")
