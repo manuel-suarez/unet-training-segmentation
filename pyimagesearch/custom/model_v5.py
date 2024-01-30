7439ef46a1be8fcad0be5d49677e7d22a2384461
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-30 09:30:11 -0600

    Added batch normalization to stabilize training

--- pyimagesearch/custom/model_v5.py
+++ pyimagesearch/custom/model_v5.py
@@ -7,14 +7,18 @@ class EncoderBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu2 = nn.ReLU()
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
         x = self.relu1(x)
         x = self.conv2(x)
+        x = self.bn1(x)
         x = self.relu2(x)
 
         return x
--- pyimagesearch/custom/model_v6.py
+++ pyimagesearch/custom/model_v6.py
@@ -6,14 +6,18 @@ class EncoderBlock(nn.Module):
     def __init__(self, in_channels=1, out_channels=64, kernel_size=3, padding=1):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu2 = nn.ReLU()
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.bn1(x)
         x = self.relu1(x)
         x = self.conv2(x)
+        x = self.bn2(x)
         x = self.relu2(x)
 
         return x
